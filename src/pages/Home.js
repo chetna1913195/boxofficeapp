278bd710d495225d1cedc894c489e9cc8963411d
chetna1913195 <saxenachetna30@gmail.com>
2021-06-14 08:39:53 +0530

    added search and input field

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 function Home() {
-  return <MainPageLayout>this is home page.</MainPageLayout>;
+  const [input, setInput] = useState('');
+  const onChangeHandler = ev => {
+    setInput(ev.target.value);
+  };
+  const onClickHandler = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json)
+      .then(result => {
+        console.log(result);
+      });
+  };
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) onClickHandler();
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onChangeHandler}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onClickHandler}>
+        search
+      </button>
+    </MainPageLayout>
+  );
 }
 
 export default Home;
