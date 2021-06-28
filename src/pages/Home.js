b96f3f7a5726119bf38a4d5a70089fb9b4d18ebf
chetna1913195 <saxenachetna30@gmail.com>
2021-06-28 16:19:34 +0530

    added cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
+import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
+
+import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
 
 function Home() {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOptions, setSearchOption] = useState('shows');
+  const isShowSearch = searchOptions === 'shows';
 
   const onClickHandler = () => {
-    apiGet(`/search/shows?q=${input}`).then(result => {
+    apiGet(`/search/${searchOptions}?q=${input}`).then(result => {
       setResults(result);
     });
   };
@@ -24,25 +29,49 @@ function Home() {
       return <div>no results</div>;
     }
     if (results && results.length > 0) {
-      return (
-        <div>
-          {results.map(item => (
-            <div key={item.show.id}>{item.show.name}</div>
-          ))}
-        </div>
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
       );
     }
     return null;
   };
-
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+  console.log(searchOptions);
   return (
     <MainPageLayout>
       <input
         type="text"
+        placeholder="Search for something"
         onChange={onChangeHandler}
         onKeyDown={onKeyDown}
         value={input}
       />
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            checked={isShowSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+        <label htmlFor="actors-search">
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
       <button type="button" onClick={onClickHandler}>
         search
       </button>
